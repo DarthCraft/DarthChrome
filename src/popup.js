eba0347fc044444cc151c611ed48f30001877cbc
Ryan <Wild1145@users.noreply.github.com>
2015-08-03 12:44:53 +0100

    Updated the voting links.

--- src/popup.js
+++ src/popup.js
@@ -4,12 +4,7 @@ var $ = function(elem) {
 
 
 
-document.addEventListener('DOMContentLoaded', function () {
-	$('playdc').addEventListener("click", function () {
-		chrome.tabs.create({url: "http://www.darthcraft.net/play"});
-		self.close();
-	});
-	
+document.addEventListener('DOMContentLoaded', function () {	
 	$('viewwebsite').addEventListener("click", function () {
 		chrome.tabs.create({url: "http://www.darthcraft.net/"});
 		self.close();
@@ -22,43 +17,35 @@ document.addEventListener('DOMContentLoaded', function () {
   
   // Voting tabs
   $('vote1').addEventListener("click", function () {
-		chrome.tabs.create({url: "https://minestatus.net/19018-darthcraft-non-pvp-survival-freebuild-no-lag/vote"});
+		chrome.tabs.create({url: "http://mclistserv.com/server/3961"});
 		self.close();
 	});
   $('vote2').addEventListener("click", function () {
-		chrome.tabs.create({url: "http://mcserverstatus.com/vote/13153"});
-		self.close();
-	});
-  $('vote3').addEventListener("click", function () {
-		chrome.tabs.create({url: "http://mclistserv.com/server/698"});
-		self.close();
-	});
-  $('vote4').addEventListener("click", function () {
-		chrome.tabs.create({url: "https://mcservers.org/vote/play.darthcraft.net"});
-		self.close();
-	});
-  $('vote5').addEventListener("click", function () {
 		chrome.tabs.create({url: "http://minecraftservers.net/server/12259/vote/"});
 		self.close();
 	});
+  $('vote3').addEventListener("click", function () {
+		chrome.tabs.create({url: "http://minecraftservers.org/vote/36130"});
+		self.close();
+	});
+  $('vote4').addEventListener("click", function () {
+		chrome.tabs.create({url: "http://minecraft-server-list.com/server/66613/vote/"});
+		self.close();
+	});
+  $('vote5').addEventListener("click", function () {
+		chrome.tabs.create({url: "http://minecraft-mp.com/server/90971/vote/"});
+		self.close();
+	});
   $('vote6').addEventListener("click", function () {
-		chrome.tabs.create({url: " http://minecraftservers.org/server/36130"});
+		chrome.tabs.create({url: "http://www.minecraftserverland.com/vote/3727"});
 		self.close();
 	});
   $('vote7').addEventListener("click", function () {
-		chrome.tabs.create({url: "http://www.theminelist.com/index.php?a=in&u=DarthSalamon"});
+		chrome.tabs.create({url: "http://www.serverpact.com/vote-26522"});
 		self.close();
 	});
   $('vote8').addEventListener("click", function () {
-		chrome.tabs.create({url: "http://topg.org/Minecraft/in-358586"});
-		self.close();
-	});
-  $('vote9').addEventListener("click", function () {
-		chrome.tabs.create({url: "http://www.mc-sl.net/index.php/serverlist/588"});
-		self.close();
-	});
-  $('vote10').addEventListener("click", function () {
-		chrome.tabs.create({url: "http://serverlistminecraft.com/?user=DarthSalamon"});
+		chrome.tabs.create({url: "http://topg.org/Minecraft/in-412037"});
 		self.close();
 	});
   
